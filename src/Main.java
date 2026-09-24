fb9f51ee620a22cf714deea640ed9afa526b7b65
Samuel Mac <samueloliveirabrito80@gmail.com>
2026-09-24 16:13:06 -0300

    style: padroniza saída do console e restaura Assinatura

--- src/Main.java
+++ src/Main.java
@@ -19,114 +19,120 @@ import java.util.List;
 
 public class Main {
 
-        public static void main(String[] args) {
-                System.out.println("Iniciando aplicativo de streaming de áudio");
+    public static void main(String[] args) {
+        System.out.println("Iniciando aplicativo de streaming de áudio");
 
-                // Criando usuários
-                UsuarioGratuito usuarioGratuito = new UsuarioGratuito(1, "Samuel", "Samuel@email.com", true);
-                UsuarioPremium usuarioPremium = new UsuarioPremium(2, "perola", "perola@email.com", 11.90);
-                UsuarioFamilia usuarioFamilia = new UsuarioFamilia(3, "Família brito", "brito@email.com", 4);
+        // Criando usuários
+        UsuarioGratuito usuarioGratuito = new UsuarioGratuito(1, "Samuel", "Samuel@email.com", true);
+        UsuarioPremium usuarioPremium = new UsuarioPremium(2, "perola", "perola@email.com", 11.90);
+        UsuarioFamilia usuarioFamilia = new UsuarioFamilia(3, "Família brito", "brito@email.com", 4);
 
-                // Criando artistas, narradores e apresentadores
-                Artista artista = new Artista(4, "Slipknot", "Slipknot@email.com", "Rock");
-                Narrador narrador = new Narrador(5, "bruno formiga", "formiga@email.com", "Empolgante");
-                Apresentador apresentador = new Apresentador(6, "clay", "clay@email.com", "De Frente com Gadu");
+        // Criando artistas, narradores e apresentadores
+        Artista artista = new Artista(4, "Slipknot", "Slipknot@email.com", "Rock");
+        Narrador narrador = new Narrador(5, "bruno formiga", "formiga@email.com", "Empolgante");
+        Apresentador apresentador = new Apresentador(6, "clay", "clay@email.com", "De Frente com Gadu");
 
-                // Exibindo informações dos usuários
-                System.out.println("\n--- Informações dos Usuários ---");
-                usuarioGratuito.exibirInfo();
-                usuarioPremium.exibirInfo();
-                usuarioFamilia.exibirInfo();
-                artista.exibirInfo();
-                narrador.exibirInfo();
-                apresentador.exibirInfo();
+        // Exibindo informações dos usuários
+        System.out.println("\n--- Informações dos Usuários ---");
+        usuarioGratuito.exibirInfo();
+        usuarioPremium.exibirInfo();
+        usuarioFamilia.exibirInfo();
+        artista.exibirInfo();
+        narrador.exibirInfo();
+        apresentador.exibirInfo();
 
-                // Criando álbuns
-                Album album1 = new Album("CHROMAKOPIA", 2024);
-                Album album2 = new Album("Lançamentos", 2025);
+        // Criando álbuns
+        Album album1 = new Album("CHROMAKOPIA", 2024);
+        Album album2 = new Album("Lançamentos", 2025);
 
-                // Composição: as músicas são criadas pelo próprio álbum
-                Musica musica = album1.addMusica(1, "Noid", 204, artista);
-                album1.addMusica(4, "Lonely day", 168, artista);
-                album1.addMusica(5, "It was a good day", 260, artista);
-                album2.addMusica(6, "Feeling Good", 172, artista);
-                album2.addMusica(7, "River", 240, artista);
+        // Composição: as músicas são criadas pelo próprio álbum
+        Musica musica = album1.addMusica(1, "Noid", 204, artista);
+        album1.addMusica(4, "Lonely day", 168, artista);
+        album1.addMusica(5, "It was a good day", 260, artista);
+        album2.addMusica(6, "Feeling Good", 172, artista);
+        album2.addMusica(7, "River", 240, artista);
 
-                // Outros conteúdos
-                Podcast podcast = new Podcast(2, "o poder so habito", 3480, apresentador, 5);
-                Audiobook audiobook = new Audiobook(3, "harry potter", 15600, narrador, 8);
-                VideoMusical videoMusical = new VideoMusical(8, "Duality", 254, artista, "1080p");
+        // Outros conteúdos
+        Podcast podcast = new Podcast(2, "o poder so habito", 3480, apresentador, 5);
+        Audiobook audiobook = new Audiobook(3, "harry potter", 15600, narrador, 8);
+        VideoMusical videoMusical = new VideoMusical(8, "Duality", 254, artista, "1080p");
 
-                // Polimorfismo: cada conteúdo reproduz do seu jeito
-                System.out.println("\n--- Manipulação de Conteúdos ---");
-                Conteudo[] conteudos = { musica, podcast, audiobook, videoMusical };
-                for (Conteudo c : conteudos) {
-                        c.reproduzir();
-                }
-
-                // Agregação: a playlist recebe conteúdos que já existem
-                System.out.println("\n--- Playlists e Histórico ---");
-                Playlist playlist = new Playlist("Minhas favoritas", usuarioPremium);
-                playlist.addConteudo(musica);
-                playlist.addConteudo(podcast);
-                playlist.addConteudo(audiobook);
-                playlist.addConteudo(videoMusical);
-                playlist.reproduzir();
-
-                Historico historico = new Historico(usuarioPremium);
-                historico.registrar(musica);
-                historico.registrar(podcast);
-                historico.registrar(videoMusical);
-                historico.exibir();
-                // Exibindo informações dos álbuns
-                System.out.println("\n--- Informações dos Álbuns ---");
-                album1.exibirInfo();
-                album2.exibirInfo();
-
-                // Processando pagamentos e assinaturas
-                System.out.println("\n--- Pagamentos e Assinaturas ---");
-                HistoricoTransacoes historicoTransacoes = new HistoricoTransacoes();
-                historicoTransacoes.registrar(new Pagamento(new Cartao("perola", "1234"), 11.90));
-                historicoTransacoes.registrar(new Pagamento(new Pix("perola@email.com"), 11.90));
-                historicoTransacoes.registrar(new Pagamento(new Boleto("34191.79001 01043.510047"), 29.90));
-                historicoTransacoes.exibir();
-
-                // Avaliações e comentários
-                System.out.println("\n--- Avaliações e Comentários ---");
-                Avaliacao avaliacao = new Avaliacao("Fantastica!", 5);
-                avaliacao.avaliar();
-
-                Comentario comentario = new Comentario("Ótimo episódio, recomendo!", usuarioPremium);
-                System.out.println("Comentário de " + comentario.getUsuario().getNome() + ": " + comentario.getTexto());
-
-                // Gêneros e recomendações
-                Genero genero = new Genero("Forró");
-                System.out.println("Gênero criado: " + genero.getNome());
-
-                // Serviços: recomendação, notificação e relatório
-                System.out.println("\n--- Recomendações, Notificações e Relatório ---");
-                List<Conteudo> catalogo = new ArrayList<>(List.of(musica, podcast, audiobook, videoMusical));
-                catalogo.addAll(album2.getMusicas());
-
-                Recomendacao recomendacao = new Recomendacao("conteúdos que você ainda não ouviu");
-                recomendacao.sugerir(historico, catalogo);
-
-                Notificacao notificacao = new Notificacao();
-                notificacao.enviar(usuarioPremium, "Seu pagamento foi aprovado!");
-                notificacao.enviar(usuarioPremium, "Novo lançamento: " + videoMusical.getTitulo());
-
-                RelatorioUso relatorio = new RelatorioUso();
-                relatorio.gerar(historico);
-
-                // Demonstração de funcionalidades específicas
-                System.out.println("\n--- Funcionalidades Específicas ---");
-                usuarioPremium.baixar("Música: River");
-                usuarioGratuito.ouvirAnuncio();
-                usuarioFamilia.adicionarMembro();
-                artista.publicarConteudo();
-                narrador.narrar();
-                apresentador.apresentar();
-
-                System.out.println("\nAplicativo encerrado com sucesso!");
+        // Polimorfismo: cada conteúdo reproduz do seu jeito
+        System.out.println("\n--- Manipulação de Conteúdos ---");
+        Conteudo[] conteudos = { musica, podcast, audiobook, videoMusical };
+        for (Conteudo c : conteudos) {
+            c.reproduzir();
         }
+
+        // Agregação: a playlist recebe conteúdos que já existem
+        System.out.println("\n--- Playlist ---");
+        Playlist playlist = new Playlist("Minhas favoritas", usuarioPremium);
+        playlist.addConteudo(musica);
+        playlist.addConteudo(podcast);
+        playlist.addConteudo(audiobook);
+        playlist.addConteudo(videoMusical);
+        playlist.reproduzir();
+
+        System.out.println("\n--- Histórico ---");
+        Historico historico = new Historico(usuarioPremium);
+        historico.registrar(musica);
+        historico.registrar(podcast);
+        historico.registrar(videoMusical);
+        historico.exibir();
+        // Exibindo informações dos álbuns
+        System.out.println("\n--- Informações dos Álbuns ---");
+        album1.exibirInfo();
+        album2.exibirInfo();
+
+        // Processando pagamentos e assinaturas
+        System.out.println("\n--- Pagamentos e Assinaturas ---");
+        HistoricoTransacoes historicoTransacoes = new HistoricoTransacoes();
+        historicoTransacoes.registrar(new Pagamento(new Cartao("perola", "1234"), 11.90));
+        historicoTransacoes.registrar(new Pagamento(new Pix("perola@email.com"), 11.90));
+        historicoTransacoes.registrar(new Pagamento(new Boleto("34191.79001 01043.510047"), 29.90));
+        historicoTransacoes.exibir();
+
+        Assinatura assinatura = new Assinatura();
+        assinatura.setTipo("Premium");
+        assinatura.setPreco(11.90);
+        assinatura.exibir();
+
+        // Avaliações e comentários
+        System.out.println("\n--- Avaliações e Comentários ---");
+        Avaliacao avaliacao = new Avaliacao("Fantastica!", 5);
+        avaliacao.avaliar();
+
+        Comentario comentario = new Comentario("Ótimo episódio, recomendo!", usuarioPremium);
+        System.out.println("Comentário de " + comentario.getUsuario().getNome() + ": " + comentario.getTexto());
+
+        // Gêneros e recomendações
+        Genero genero = new Genero("Forró");
+        System.out.println("Gênero criado: " + genero.getNome());
+
+        // Serviços: recomendação, notificação e relatório
+        System.out.println("\n--- Recomendações, Notificações e Relatório ---");
+        List<Conteudo> catalogo = new ArrayList<>(List.of(musica, podcast, audiobook, videoMusical));
+        catalogo.addAll(album2.getMusicas());
+
+        Recomendacao recomendacao = new Recomendacao("conteúdos que você ainda não ouviu");
+        recomendacao.sugerir(historico, catalogo);
+
+        Notificacao notificacao = new Notificacao();
+        notificacao.enviar(usuarioPremium, "Seu pagamento foi aprovado!");
+        notificacao.enviar(usuarioPremium, "Novo lançamento: " + videoMusical.getTitulo());
+
+        RelatorioUso relatorio = new RelatorioUso();
+        relatorio.gerar(historico);
+
+        // Demonstração de funcionalidades específicas
+        System.out.println("\n--- Funcionalidades Específicas ---");
+        usuarioPremium.baixar("Música: River");
+        usuarioGratuito.ouvirAnuncio();
+        usuarioFamilia.adicionarMembro();
+        artista.publicarConteudo();
+        narrador.narrar();
+        apresentador.apresentar();
+
+        System.out.println("\nAplicativo encerrado com sucesso!");
+    }
 }
